6aa60793ddbb1285654d378f1c94b2aac3bd8e32
Steven Hansen <zergylord@gmail.com>
2016-03-28 13:25:29 -0700

    first iteration of qnet combolock, doesnt work well yet..

--- mnist/combolock.lua
+++ mnist/combolock.lua
@@ -28,6 +28,20 @@ softmax = nn.SoftMax():cuda()
 local num_steps = 1e5
 local cumloss =0 
 
+use_qnet = true
+if use_qnet then
+    local hid_dim = 100
+    local input = nn.Identity():cuda()()
+    --local hid = nn.BatchNormalization(hid_dim):cuda()(nn.ReLU():cuda()(nn.Linear(in_dim,hid_dim):cuda()(input)))
+    local hid = nn.ReLU():cuda()(nn.Linear(in_dim,hid_dim):cuda()(input))
+    local output =nn.Linear(hid_dim,act_dim):cuda()(hid)
+    q_network = nn.gModule({input},{output})
+    q_w,q_dw = q_network:getParameters()
+    q_config = {
+        learningRate  = 1e-4
+        }
+        mse_crit = nn.MSECriterion():cuda()
+end
 
 
 
@@ -47,11 +61,12 @@ end
 
 
 s = 1
+s_digit = digit[s][torch.random(digit[s]:size(1))]
 epsilon = .1
 alpha = .1
 gamma = .9
 net_reward = 0
-refresh = 1e2
+refresh = 1e3
 bonus_hist = torch.zeros(num_steps/refresh)
 C = torch.zeros(mb_dim)
 neg_entropy = torch.zeros(num_state,act_dim)
@@ -71,7 +86,8 @@ D.s = torch.zeros(D.size)
 D.a = torch.zeros(D.size)
 D.r = torch.zeros(D.size)
 D.sPrime = torch.zeros(D.size)
-D.digit = torch.zeros(D.size,digit[1]:size(2))--sPrime digit
+D.digit = torch.zeros(D.size,digit[1]:size(2))--s digit
+D.digitPrime = torch.zeros(D.size,digit[1]:size(2))--sPrime digit
 D.i = 1
 local get_data = function(data,action_data)
     num = data:size(1)
@@ -85,14 +101,29 @@ local get_data = function(data,action_data)
 end
 set_data_func(get_data)
 final_time = -1
+state = torch.zeros(mb_dim*act_dim,in_dim)
+statePrime = torch.zeros(mb_dim,in_dim)
+aind = torch.LongTensor(mb_dim*act_dim,1)
+for a=1,act_dim do
+    for i=1,mb_dim do
+        aind[mb_dim*(a-1)+i] = a
+    end
+end
 for t=1,num_steps do
     r = 0
     --select action
-    _,a = torch.max(Q[s],1)
+    if use_qnet then
+        local vals = q_network:forward(s_digit:view(1,in_dim):cuda())
+        _,a = vals:max(2)
+        a = a[1]
+    else
+        _,a = torch.max(Q[s],1)
+    end
     a = a[1]
 
     --perform action
     sPrime = T[s][a]
+    sPrime_digit = digit[sPrime][torch.random(digit[sPrime]:size(1))]
     sa_visits[s][a] = sa_visits[s][a] + 1
     visits[sPrime] = visits[sPrime] + 1
 
@@ -105,7 +136,8 @@ for t=1,num_steps do
     D.a[D.i] = a
     D.r[D.i] = r
     D.sPrime[D.i] = sPrime
-    D.digit[D.i] = digit[sPrime][torch.random(digit[sPrime]:size(1))] 
+    D.digit[D.i] = s_digit:clone() 
+    D.digitPrime[D.i] = sPrime_digit:clone() 
     D.i = (D.i % D.size) + 1
 
     --update model params
@@ -120,24 +152,33 @@ for t=1,num_steps do
                 action[mb_dim*(a-1)+i][a] = 1-torch.rand(1):mul(noise_mag)[1]
             end
         end
+        mind = mask:nonzero()
         --update adver nets
         _,batchloss = optim.adam(train,w,config)
         cumloss = cumloss + batchloss[1]
         --update Q
         local x,y,target 
-        if use_qnet then
-            x = torch.zeros(mb_dim*act_dim,in_dim)
-            y = q_network:forward()
-            target = torch.zeros(mb_dim*act_dim,act_dim)
-        end
         local s,a,r,sPrime,a_actual
         s = D.s[mask:squeeze()]
         s = s:repeatTensor(act_dim)
         sPrime = D.sPrime[mask:squeeze()]
         r = D.r[mask:squeeze()]
         a_actual = D.a[mask:squeeze()]
-        state = D.digit[mask:expandAs(D.digit)]:reshape(mb_dim,in_dim)
-        state = state:repeatTensor(act_dim,1)
+        --
+        for i=1,mb_dim do
+            for a=1,act_dim do
+                state[mb_dim*(a-1)+i] = D.digit[mind[i][1] ]
+            end
+            statePrime[i] = D.digitPrime[mind[i][1] ]
+        end
+        --]]
+        --state = D.digit[mask:expandAs(D.digit)]:reshape(mb_dim,in_dim):repeatTensor(act_dim,1)
+        --statePrime = D.digitPrime[mask:expandAs(D.digitPrime)]:reshape(mb_dim,in_dim):repeatTensor(act_dim,1)
+        if use_qnet then
+            qPrime,qind = q_network:forward(statePrime:cuda()):max(2)
+        else
+            Q_clone = Q:clone()
+        end
 
         network:evaluate()
         local possible
@@ -148,24 +189,20 @@ for t=1,num_steps do
         end
         C = network:forward(possible):squeeze()
         target = torch.zeros(mb_dim*act_dim)
-        target_mask = torch.zeros(mb_dim*act_dim):byte()
-        Q_clone = Q:clone()
+        target_mask = torch.zeros(mb_dim*act_dim,1):byte()
         for i=1,mb_dim do
             --you can experience all actions under threshold, since they all go to heaven!
             local known_flag = true
             for a = 1,act_dim do
                 local ind = mb_dim*(a-1)+i
                 hist_total[s[i] ][a] = hist_total[s[i] ][a] + 1
-                local chance_unknown = (1 - H(C[mb_dim*(a-1)+i]))^(1/temp)
+                local chance_unknown = (1 - H(C[ind]))^(1/temp)
                 neg_entropy[s[i] ][a] = neg_entropy[s[i] ][a] + chance_unknown
                 if  chance_unknown > torch.rand(1)[1] then
                     if a == a_actual[i] then--D.a[mb_ind[i] ] then
                         known_flag = false
                     end
                     local r = 1
-                    if use_qnet then
-                        target = r - y
-                    end
                     target[ind] = r
                     target_mask[ind] = 1
                 end
@@ -179,30 +216,72 @@ for t=1,num_steps do
                 if s[i] == num_state then
                     target[ind] = r[i]
                 else
-                    target[ind] = r[i]+gamma*torch.max(Q_clone[sPrime[i]])
+                    if use_qnet then
+                        target[ind] = r[i]+gamma*torch.max(qPrime[i])
+                    else
+                        target[ind] = r[i]+gamma*torch.max(Q_clone[sPrime[i]])
+                    end
                 end
             end
         end
-        for i=1,mb_dim do
-            for a=1,act_dim do
-                local ind = mb_dim*(a-1)+i
-                if target_mask[ind] == 1 then
-                    Q[s[i] ][a] = (1-alpha)*Q[s[i] ][a] + (alpha)*target[ind]
+        if use_qnet then
+            local function q_train(x)
+            if x ~= q_w then
+                q_w:copy(x)
+            end
+            q_network:zeroGradParameters()
+            local o = q_network:forward(state:cuda())
+            local used = o:gather(2,aind:cuda())
+            local loss = mse_crit:forward(used,target:cuda())
+            local grad = mse_crit:backward(used,target:cuda())
+            local act_grad = torch.zeros(act_dim*mb_dim,act_dim):cuda()
+            for i=1,mb_dim*act_dim do
+                if target_mask[i][1] == 1 then
+                    act_grad[i][aind[i][1]] = grad[i]
+                end
+            end
+            q_network:backward(state,act_grad)
+            return loss,q_dw
+            end
+            _,batchloss = optim.adam(q_train,q_w,q_config)
+        else
+            for i=1,mb_dim do
+                for a=1,act_dim do
+                    local ind = mb_dim*(a-1)+i
+                    if target_mask[ind][1] == 1 then
+                        Q[s[i] ][a] = (1-alpha)*Q[s[i] ][a] + (alpha)*target[ind]
+                    end
                 end
             end
         end
     end
     s = sPrime
-
+    s_digit = sPrime_digit:clone() 
     if t % refresh == 0 then
         gnuplot.figure(1)
         gnuplot.raw("set multiplot layout 2,4 columnsfirst")
 
 
-        gnuplot.raw("set title 'Q-values' ")
-        gnuplot.raw('set xrange [' .. .5 .. ':' .. num_state+.5 .. '] noreverse')
-        gnuplot.raw('set yrange [0:1] noreverse')
-        gnuplot.plot({Q:mean(2)},{Q:max(2):double()},{Q:min(2):double()})
+        if use_qnet then
+            local data = torch.zeros(num_state,in_dim):cuda()
+            for s = 1,num_state do
+                data[s] = digit[s][torch.random(digit[s]:size(1))]
+            end
+            local Q = q_network:forward(data)
+            gnuplot.raw("set title 'Q-values' ")
+            gnuplot.raw('set xrange [' .. .5 .. ':' .. num_state+.5 .. '] noreverse')
+            gnuplot.raw('set yrange [-1:1] noreverse')
+            gnuplot.plot({Q:mean(2)},{Q:max(2):double()},{Q:min(2):double()})
+            --gnuplot.imagesc(Q)
+        else
+            --
+            gnuplot.raw("set title 'Q-values' ")
+            gnuplot.raw('set xrange [' .. .5 .. ':' .. num_state+.5 .. '] noreverse')
+            gnuplot.raw('set yrange [0:1] noreverse')
+            gnuplot.plot({Q:mean(2)},{Q:max(2):double()},{Q:min(2):double()})
+            --]]
+            --gnuplot.imagesc(Q)
+        end
 
 
         
@@ -283,7 +362,7 @@ for t=1,num_steps do
             end
         end
         network:training()
-
+        
         gnuplot.raw("set title 'current D estimates' ")
         gnuplot.imagesc(cur_known)
         gnuplot.raw("set title 'total visits' ")
